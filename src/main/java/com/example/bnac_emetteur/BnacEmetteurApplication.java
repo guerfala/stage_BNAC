b2c7503945e61b75fcd51e9ebef995dc6ad1d60c
rihem09 <rihem.benaissa@esprit.tn>
2024-06-10 10:48:59 +0100

    first commit

---
+++ src/main/java/com/example/bnac_emetteur/BnacEmetteurApplication.java
@@ -0,0 +1,13 @@
+package com.example.bnac_emetteur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnacEmetteurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BnacEmetteurApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bnac_emetteur/BnacEmetteurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bnac_emetteur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnacEmetteurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
